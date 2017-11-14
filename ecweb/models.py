d9eed75688cd675e5ed6a5f006d15b8e8d622b2d
jamesperes <james.peres.s@gmail.com>
2017-11-14 03:23:52 +0000

    change Student for Use

--- ecweb/models.py
+++ ecweb/models.py
@@ -29,7 +29,7 @@ class MyUserManager(BaseUserManager):
         return user
 
 
-class Student(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
