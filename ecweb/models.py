56f4bef330013c6b56bc2a5d623e84e437fe6aa9
jamesperes <james.peres.s@gmail.com>
2017-11-15 10:14:07 +0000

    fix bug models

--- ecweb/models.py
+++ ecweb/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    date_joined = models.DateTimeField(_('date joined'))
+    date_joined = models.DateTimeField(_('date joined'), null=True)
     is_active = models.BooleanField(_('active'), default=True)
     is_staff = models.BooleanField(_('staff'), default=False)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
