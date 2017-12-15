39fdc99529947b01975890e86872b67b7c97c75d
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-14 21:54:11 -0300

    Fixing user creation errors

--- ecweb/models.py
+++ ecweb/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.mail import send_mail
+from django.utils import timezone
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
@@ -14,25 +15,35 @@ class Attendance(models.Model):
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def _create_user(self,
+                     email,
+                     password,
+                     is_staff,
+                     is_superuser,
+                     **extra_fields
+                     ):
+        now = timezone.now()
         if not email:
             raise ValueError('Users must have an email address')
-
         user = self.model(
-            email=self.normalize_email(email),
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
         )
-
         user.set_password(password)
-        user.is_staff = False
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password)
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self._create_user(
+            email, password, True, True, **extra_fields)
         return user
 
 
