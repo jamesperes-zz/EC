173a3e8f46b349b2ad116cceb4877ff731a3f2b1
samuel.sampaio <samuel.sampaio@tivit.com>
2017-12-24 04:09:55 -0200

    Refactor of models to simplify the entities :fire:

--- ecweb/models.py
+++ ecweb/models.py
@@ -1,105 +1,51 @@
 from django.db import models
 from django.core.mail import send_mail
-from django.utils import timezone
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 from .utils.li import level_choices, type_list, test_choices
 
 
-class MyUserManager(BaseUserManager):
-    def _create_user(self,
-                     email,
-                     password,
-                     is_staff,
-                     is_superuser,
-                     **extra_fields
-                     ):
-        now = timezone.now()
-        if not email:
-            raise ValueError('Users must have an email address')
-        user = self.model(
-            email=email,
-            is_staff=is_staff,
-            is_active=True,
-            is_superuser=is_superuser,
-            last_login=now,
-            date_joined=now,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        user = self._create_user(
-            email, password, True, True, **extra_fields)
-        return user
+from django.contrib.auth.models import AbstractUser
 
 
-class StudentTests(models.Model):
-    date_test = models.DateField(blank=True, null=True)
-    grade = models.FloatField(blank=True, null=True)
+class BasicUser(AbstractUser):
+    email = models.EmailField(_('email address'), null=False, blank=False, unique=True)
 
-    def __str__(self):
-        return self.date_test
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        blank=True,
+        null=True)
 
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    cod = models.IntegerField(blank=True, null=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    date_joined = models.DateTimeField(_('date joined'), null=True)
-    is_active = models.BooleanField(_('active'), default=True)
-    is_staff = models.BooleanField(_('staff'), default=False)
     avatar = models.ImageField(
         upload_to='avatars/profiles',
         default='avatars/user_default.png',
-        blank=True
-    )
-    type_of_course = models.CharField(max_length=30, choices=type_list,
-                                      blank=True)
-    objects = MyUserManager()
+        blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-
-    def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    def get_short_name(self):
-        return self.first_name
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        send_mail(subject, message, from_email, [self.email], **kwargs)
-
-    def __str__(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-
 
 class Student(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(BasicUser, on_delete=models.CASCADE)
+    cod = models.IntegerField(blank=False, null=False)
+    type_of_course = models.CharField(max_length=30, choices=type_list, blank=False, null=False)
 
     def __str__(self):
         return self.user.first_name
 
 
 class Teacher(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    user = models.OneToOneField(BasicUser, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.first_name
+
+
+
+class Coordinator(models.Model):
+    user = models.OneToOneField(BasicUser, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.first_name
+
 
 
 class ClassRoom(models.Model):
@@ -154,34 +100,3 @@ class TestGrade(models.Model):
     test_event = models.ForeignKey(Test, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     grade = models.FloatField()
-
-
-class Menssage(models.Model):
-    menssage_text = models.TextField(blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.first_name
-
-
-class Confirmed(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    go = models.BooleanField(default=False)
-    dgo = models.BooleanField(default=False)
-    dknow = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.user.first_name
-
-
-class Calendar(models.Model):
-    event = models.TextField(blank=True)
-    date_start = models.DateField(blank=True, null=True,)
-    date_end = models.DateField(blank=True, null=True,)
-    title = models.CharField(max_length=30, blank=True)
-    local = models.CharField(max_length=30, blank=True)
-    menssage = models.ManyToManyField(Menssage, blank=True)
-    confirm = models.ManyToManyField(Confirmed, blank=True)
-
-    def __str__(self):
-        return self.title
--- ecweb/views.py
+++ ecweb/views.py
@@ -1,37 +1,54 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
 from datetime import date
 
 from django.contrib.auth import logout
 
 from .forms import PhotoForm, AttendanceForm
-from .models import Calendar, Menssage, User, ClassRoom, Teacher, Student, Class
+from .models import ClassRoom, Teacher, Student, Class, BasicUser, Coordinator
 
 
 @login_required
 def home_dashboard(request):
     current_user = request.user
-    user = User.objects.get(id=current_user.id)
-    date_start = user.date_joined.date()
-    days_con = date.today() - date_start
-    days_cont_int = int(days_con.days)
-    if user.type_of_course == "1-month":
-        count_day = 30 - days_cont_int
-        percent = int(-100.0 * (count_day / 30))
+    user = Coordinator.objects.filter(user__id=current_user.id)
 
-    else:
-        count_day = 30 * 6 - days_cont_int
-        percent = int(100.0 * (count_day / (30 * 6)))
+    if user:
+        return render(request, 'ecweb/coordinator-dashboard.html',
+                      {'coordinator': user.first(),
+                       'current_user': current_user})
 
-    return render(request, 'ecweb/dashboard.html',
-                  {'current_user': current_user, 'days_cont_int': days_cont_int})
+    user = Teacher.objects.filter(user__id=current_user.id)
+    if user:
+        return render(request, 'ecweb/teacher-dashboard.html',
+                      {'teacher': user.first(),
+                       'current_user': current_user})
 
+    user = Student.objects.filter(user__id=current_user.id)
+    if user:
+        date_start = current_user.date_joined.date()
+        days_con = date.today() - date_start
+        days_cont_int = int(days_con.days)
+        if user[0].type_of_course == "1-month":
+            count_day = 30 - days_cont_int
+            percent = int(-100.0 * (count_day / 30))
+
+        else:
+            count_day = 30 * 6 - days_cont_int
+            percent = int(100.0 * (count_day / (30 * 6)))
+
+        return render(request, 'ecweb/student-dashboard.html',
+                      {'student': user.first(),
+                       'current_user': current_user,
+                       'days_cont_int': days_cont_int})
+
+    raise Http404
 
 @login_required
 def user_detail(request):
     current_user = request.user
-    insta = get_object_or_404(User, pk=int(current_user.id))
+    insta = get_object_or_404(BasicUser, pk=int(current_user.id))
     if request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES, instance=insta)
         if form.is_valid():
