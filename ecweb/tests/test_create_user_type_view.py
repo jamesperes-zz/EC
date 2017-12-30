0bbb768e1a454970275f2babe42a119825d592f2
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-30 16:32:51 -0300

    Adding TestCreateUserTypeView class, and create setUp function

---
+++ ecweb/tests/test_create_user_type_view.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+from ecweb.models import (
+    ClassRoom,
+    BasicUser,
+    Student,
+    Teacher
+)
+
+
+class TestCreateUserTypeView(TestCase):
+    """ Testing createuser system """
+
+    def setUp(self):
+        BasicUser.objects.create_superuser(
+            username='admin',
+            password='1234abc',
+            email="admin_test@admin.com"
+        )
+        self.data = {
+            'first_name': 'admin',
+            'last_name': 'test',
+            'email': 'admin_test@test.com',
+            'password': '123456ab',
+            'confirm_password': '123456ab'
+        }
+        self.url_coordinator = r('create-user', 'coordinator')
+        self.url_teacher = r('create-user', 'teacher')
+
+        self.client.login(
+            username='admin_test@admin.com',
+            password='1234abc'
+        )
