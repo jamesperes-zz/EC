84c18c0bc671050e40041a2461064d8aceddbc91
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-30 17:01:23 -0300

    Adding test_create_object_type_teacher and test_create_object_type_coordinator

--- ecweb/tests/test_create_user_type_view.py
+++ ecweb/tests/test_create_user_type_view.py
@@ -4,7 +4,8 @@ from ecweb.models import (
     ClassRoom,
     BasicUser,
     Student,
-    Teacher
+    Teacher,
+    Coordinator
 )
 
 
@@ -12,15 +13,23 @@ class TestCreateUserTypeView(TestCase):
     """ Testing createuser system """
 
     def setUp(self):
-        BasicUser.objects.create_superuser(
+        user = BasicUser.objects.create_superuser(
             username='admin',
             password='1234abc',
             email="admin_test@admin.com"
         )
-        self.data = {
+        Coordinator.objects.create(user=user)
+        self.data_coordinator = {
             'first_name': 'admin',
             'last_name': 'test',
             'email': 'admin_test@test.com',
+            'password': '123456ab',
+            'confirm_password': '123456ab'
+        }
+        self.data_teacher = {
+            'first_name': 'admin2',
+            'last_name': 'test2',
+            'email': 'admin_test2@test.com',
             'password': '123456ab',
             'confirm_password': '123456ab'
         }
@@ -39,3 +48,25 @@ class TestCreateUserTypeView(TestCase):
     def test_response_status_with_type_teacher(self):
         response = self.client.get(self.url_teacher)
         self.assertEqual(200, response.status_code)
+
+    def test_create_object_type_coordinator(self):
+        self._test_create_object(
+            data=self.data_coordinator,
+            url=self.url_coordinator,
+            model=Coordinator,
+        )
+
+    def test_create_object_type_teacher(self):
+        self._test_create_object(
+            data=self.data_teacher,
+            url=self.url_teacher,
+            model=Teacher,
+        )
+
+    def _test_create_object(self, data, model, url):
+        response = self.client.post(url, data)
+        instance = model.objects.get(
+            user__email=data['email']
+        )
+        self.assertIsInstance(instance, model)
+        self.assertRedirects(response, r('home_dashboard'))
